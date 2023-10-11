12979f127655981b7011667dcbd26920873b4bda
Stocil <vashurov02@mail.ru>
2023-10-11 21:38:49 +0300

    react reset, start learning

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,53 +1,53 @@
-import React, { Component } from "react";
-import "./Article.css";
+// import React, { Component } from "react";
+// import "./Article.css";
 
-class Article extends Component {
-  // Первый способ задания состояния
-  // constructor(props) {
-  //   super(props);
+// class Article extends Component {
+//   // Первый способ задания состояния
+//   // constructor(props) {
+//   //   super(props);
 
-  //   this.state = {
-  //     isShow: true,
-  //   };
-  // }
+//   //   this.state = {
+//   //     isShow: true,
+//   //   };
+//   // }
 
-  // Второй способ задания состояния
-  state = {
-    isShow: true,
-  };
+//   // Второй способ задания состояния
+//   state = {
+//     isShow: true,
+//   };
 
-  render() {
-    const { article } = this.props;
+//   render() {
+//     const { article } = this.props;
 
-    if (!this.state.isShow)
-      return (
-        <button className="btn btn-primary btn-lg" onClick={this.hideContent}>
-          {this.state.isShow ? "Скрыть" : "Показать"}
-        </button>
-      );
+//     if (!this.state.isShow)
+//       return (
+//         <button className="btn btn-primary btn-lg" onClick={this.hideContent}>
+//           {this.state.isShow ? "Скрыть" : "Показать"}
+//         </button>
+//       );
 
-    return (
-      <div className="card">
-        <div className="card-header">
-          <h2>{article.subtitle}</h2>
-        </div>
+//     return (
+//       <div className="card">
+//         <div className="card-header">
+//           <h2>{article.subtitle}</h2>
+//         </div>
 
-        <div className="card-body">
-          <section className="body">{article.text}</section>
+//         <div className="card-body">
+//           <section className="body">{article.text}</section>
 
-          <button className="btn btn-primary btn-lg" onClick={this.hideContent}>
-            {this.state.isShow ? "Скрыть" : "Показать"}
-          </button>
-        </div>
-      </div>
-    );
-  }
+//           <button className="btn btn-primary btn-lg" onClick={this.hideContent}>
+//             {this.state.isShow ? "Скрыть" : "Показать"}
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
 
-  hideContent = (event) => {
-    this.setState({
-      isShow: !this.state.isShow,
-    });
-  };
-}
+//   hideContent = (event) => {
+//     this.setState({
+//       isShow: !this.state.isShow,
+//     });
+//   };
+// }
 
-export default Article;
+// export default Article;
